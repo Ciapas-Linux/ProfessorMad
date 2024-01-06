fac0435e384635bf38a64b65e20a72d327d3a797
Ciapas-Linux <maxgon@o2.pl>
2024-01-06 08:32:55 +0100

    Change delta to _delta

--- Scenes/Enemies/SeeCast2D.gd
+++ Scenes/Enemies/SeeCast2D.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	pass 
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 #	if is_colliding():
 #		if get_collider().name == "Player":
 #			print("enemy: ray hit Player")
