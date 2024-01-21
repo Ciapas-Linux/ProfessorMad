011c51c64fb9bebf49ee9a05b47b76b9812779a8
Ciapas-Linux <maxgon@o2.pl>
2024-01-21 10:22:48 +0100

    Disable processing dummy gun node

--- Scenes/Weapons/Empty/Empty_gun.gd
+++ Scenes/Weapons/Empty/Empty_gun.gd
@@ -11,10 +11,10 @@ signal fire
 
 # Disable all processing this dummy node!
 func _ready() -> void:
-	#set_process_input(false)
-	#set_process_unhandled_input(false)
-	#set_process(false)
-	pass
+	set_process_input(false)
+	set_process_unhandled_input(false)
+	set_process(false)
+	
 	
 
 
